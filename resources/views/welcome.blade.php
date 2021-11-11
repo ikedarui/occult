258a030ccdc19a387d9be2b4e3a9d981774d2a23
池田琉生 <ruisandes@i.softbank.jp>
2021-11-11 19:32:36 +0000

    トップページ

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,92 +8,37 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        
+        <link href="{{ secure_asset('css/welcome.css') }}" rel="stylesheet">
+        
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
+            <img src="https://www.pakutaso.com/shared/img/thumb/006horror0523_TP_V.jpg" width="1400" height="900" alt="女の霊">
                 <div class="title m-b-md">
-                    Laravel
+                    オカルトクラブ
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                @if (Route::has('login'))
+                    <div class="links">
+                        <ul>
+                        @auth
+                            <li class="okura"><a href="{{ url('/home') }}">Home</a></li>
+                        @else
+                            <li class="negitama"><a href="{{ route('login') }}">Login</a></li>
+    
+                            @if (Route::has('register'))
+                                <li class="ti-gyu"><a href="{{ route('register') }}">Register</a></li>
+                            @endif
+                        @endauth
+                        </ul>
+                    </div>
+                @endif
             </div>
         </div>
     </body>
+    <footer>
+        <p class="copyright">(C) 2021 心霊現象研究委員会　代表　ガリボネ.</p>
+    </footer>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .js('resources/js/front.js', 'public/js')
+   .js('resources/js/comment.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
     .sass('resources/sass/new.scss', 'public/css')
     .sass('resources/sass/front.scss','public/css')
-    .sass('resources/sass/admin.scss','public/css');
+    .sass('resources/sass/admin.scss','public/css')
+    .sass('resources/sass/welcome.scss','public/css');
+    
